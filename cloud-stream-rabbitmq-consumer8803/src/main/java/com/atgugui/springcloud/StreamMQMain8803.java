0899c34b54e130989cdb87881456d1554e38542f
Bokhoo <13770620@qq.com>
2020-12-08 10:54:02 +0800

    rabbitmq8803 and stream 消息驱动group分组实现消息竞争,持久化

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atgugui/springcloud/StreamMQMain8803.java
@@ -0,0 +1,17 @@
+package com.atgugui.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zbh
+ * @date 2020/12/8
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/IMessageProvider.java
@@ -0,0 +1,11 @@
+package com.atguigu.springcloud.service;
+
+/**
+ * @author zbh
+ * @date 2020/12/7
+ */
+public interface IMessageProvider {
+
+    public String send();
+
+}
